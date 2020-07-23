83c82d67b3b57d28675f59c1b6d8944770f35779
whywelive <hello@whywelive.me>
2020-07-23 14:11:16 +0700

    Initial Commit

---
+++ Shared/cs_appleApp.swift
@@ -0,0 +1,17 @@
+//
+//  cs_appleApp.swift
+//  Shared
+//
+//  Created by whywelive on 23.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct cs_appleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
