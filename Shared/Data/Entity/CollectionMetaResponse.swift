9700cab1e9ebb6b5927181eb9ac90bd76596daea
whywelive <hello@whywelive.me>
2020-08-17 15:26:31 +0700

    chore: project upload

---
+++ Shared/Data/Entity/CollectionMetaResponse.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+struct CollectionMetaResponse<T: Codable>: Codable {
+    let meta: CollectionMeta
+    let items: [T]
+    
+    struct CollectionMeta: Codable {
+        let count: Int
+        let totalCount: Int?
+    }
+}
