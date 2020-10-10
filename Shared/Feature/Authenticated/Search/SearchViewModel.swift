4028ae7d770769f42bbadd98532e8049c8cd42c2
WhyWeLive <hello@whywelive.me>
2020-10-10 14:03:44 +0700

    feat(search): work with api

---
+++ Shared/Feature/Authenticated/Search/SearchViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SearchViewModel.swift
+//  iOS
+//
+//  Created by whywelive on 10.10.2020.
+//
+
+import Foundation
---
+++ Shared/Networking/CollectionResponse.swift
@@ -0,0 +1,8 @@
+//
+//  CollectionResponse.swift
+//  iOS
+//
+//  Created by whywelive on 10.10.2020.
+//
+
+import Foundation
