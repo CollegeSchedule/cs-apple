5543a130bef667145a5e3ed826930389be4c8419
EverlastingHolder <moshkovcevroma@gmail.com>
2020-10-21 13:49:03 +0700

    feat(schedule): create base code

---
+++ Shared/Feature/Authenticated/Schedule/ScheduleItemView.swift
@@ -0,0 +1,8 @@
+//
+//  ScheduleItemView.swift
+//  iOS
+//
+//  Created by admin on 21.10.2020.
+//
+
+import Foundation
---
+++ Shared/Feature/Authenticated/Schedule/ScheduleView.swift
@@ -0,0 +1,8 @@
+//
+//  ScheduleView.swift
+//  iOS
+//
+//  Created by admin on 21.10.2020.
+//
+
+import Foundation
