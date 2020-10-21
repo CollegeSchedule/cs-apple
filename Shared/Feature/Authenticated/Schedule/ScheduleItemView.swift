d3910370841cb93634966c3e92164a3e6f953baa
EverlastingHolder <moshkovcevroma@gmail.com>
2020-10-21 13:49:36 +0700

    feat(schedule): create base code

--- Shared/Feature/Authenticated/Schedule/ScheduleItemView.swift
+++ Shared/Feature/Authenticated/Schedule/ScheduleItemView.swift
@@ -1,8 +1,34 @@
-//
-//  ScheduleItemView.swift
-//  iOS
-//
-//  Created by admin on 21.10.2020.
-//
+import SwiftUI
 
-import Foundation
+struct ScheduleItemView: View {
+    let item: ScheduleView.ScheduleItem
+    
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text(self.item.startLes)
+                Text(self.item.endLes)
+            }
+            .padding(.trailing)
+            
+            HStack{
+                Circle()
+                    .frame(width: 10, height: 10)
+                    .foregroundColor(.green)
+                
+                VStack(alignment: .leading) {
+                    Text(self.item.lesson)
+                    Text("Аудитория: \(self.item.classroom)")
+                }
+            }
+            Spacer()
+            
+            VStack(alignment: .center) {
+                Circle()
+                    .frame(width: 25, height: 25)
+                    .foregroundColor(.green)
+            }
+        }
+        .padding()
+    }
+}
--- Shared/Feature/Authenticated/Schedule/ScheduleView.swift
+++ Shared/Feature/Authenticated/Schedule/ScheduleView.swift
@@ -1,8 +1,109 @@
-//
-//  ScheduleView.swift
-//  iOS
-//
-//  Created by admin on 21.10.2020.
-//
+import SwiftUI
 
-import Foundation
+struct ScheduleView: View {
+    let week: [String] = ["пн","вт","ср","чт","пт","сб","вс"]
+    
+    @State
+    private var items: [ScheduleItem] = [
+        .init(
+            startLes: "10:50",
+            endLes: "12:00",
+            lesson: "History",
+            classroom: "309"
+        ),
+        .init(
+            startLes: "10:50",
+            endLes: "12:00",
+            lesson: "History",
+            classroom: "309"
+        ),
+        .init(
+            startLes: "10:50",
+            endLes: "12:00",
+            lesson: "History",
+            classroom: "309"
+        ),
+        .init(
+            startLes: "10:50",
+            endLes: "12:00",
+            lesson: "History",
+            classroom: "309"
+        ),
+        .init(
+            startLes: "10:50",
+            endLes: "12:00",
+            lesson: "History",
+            classroom: "309"
+        ),
+        .init(
+            startLes: "10:50",
+            endLes: "12:00",
+            lesson: "History",
+            classroom: "309"
+        )
+    ]
+    
+    var body: some View {
+        VStack{
+            Divider()
+                .padding(.horizontal)
+            VStack{
+                ScrollView(.horizontal, showsIndicators: false ) {
+                    HStack{
+                        ForEach(self.getDate(), id: \.self) { day in
+                            VStack{
+                                Text(day)
+                                    .font(.title2)
+                                Text(week[0])
+                                    .font(.title3)
+                            }
+                        }
+                    }
+                }
+            }
+            .padding()
+            
+            VStack{
+                Divider()
+                HStack{
+                    Text("Monday, 19 october")
+                    Spacer()
+                    Text("Week")
+                }
+                .padding(.horizontal)
+                Divider()
+            }
+            
+            ScrollView(showsIndicators: false) {
+                ForEach(self.items, id: \.self){ item in
+                    ScheduleItemView(item: item)
+                }
+            }
+            Spacer()
+        }
+    }
+    
+    struct ScheduleItem: Hashable{
+        let startLes: String
+        let endLes: String
+        let lesson: String
+        let classroom: String
+    }
+    
+    func getDate() -> [String] {
+        let date = Date()
+        let formatter = DateFormatter()
+        var dateWeek: [String] = []
+        
+        formatter.locale = Locale(identifier: "nl_NL")
+        formatter.setLocalizedDateFormatFromTemplate("dd")
+        
+        
+        for i in 0...14{
+            let nextDay = Calendar.current.date(byAdding: .day, value: i, to: date)
+            dateWeek.append(formatter.string(from: nextDay!))
+        }
+
+        return dateWeek
+    }
+}
